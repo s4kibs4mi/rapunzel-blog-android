f6f33fa7790274ce9a0453300019f6c26c5d7573
Sakib Sami <s4kibs4mi@gmail.com>
2018-02-22 16:44:31 +0600

    Application Bootstrapped

---
+++ app/src/main/java/ninja/sakib/rapunzel/android/ui/activities/SplashActivity.kt
@@ -0,0 +1,14 @@
+package ninja.sakib.rapunzel.android.ui.activities
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import ninja.sakib.rapunzel.android.R
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
+
